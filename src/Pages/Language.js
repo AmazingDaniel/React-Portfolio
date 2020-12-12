b8b85ab7a6e8c75b84ca5593956a3816e79c43d2
Daniel Moreno <drmjr417@sbcglobal.net>
2020-12-12 13:44:39 -0600

    Created my Language page

---
+++ src/Pages/Language.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../styles/style.css";
+import Container from 'react-bootstrap/Container';
+
+
+const Language = () => (
+    <Container id="container">
+        
+        <h3>Programming skills and proficiencies</h3>
+        <div id="language">
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>Javascript</li>
+                <li>jQuery</li>
+                <li>APIs</li>
+                <li>Nodejs</li>
+                <li>O.O.P.</li>
+                <li>Express</li>
+                <li>SQL</li>
+                <li>MVC</li>
+                <li>Mongodb</li>
+                <li>PWA</li>
+                <li>React</li>
+                <li>M.E.R.N</li>
+            </ul>
+        </div>
+
+    </Container>
+);
+
+export default Language;
